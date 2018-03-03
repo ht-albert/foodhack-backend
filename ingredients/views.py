02def1d250c35989195877fc9d67aeddeb225bc8
Альберт Хачатрян <ht.albert@MacBook-Pro-Albert.local>
2018-03-03 20:02:25 +0300

    add csrf

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,8 +1,10 @@
+from django.views.decorators.csrf import csrf_protect
 from django.http import JsonResponse
 from ingredients.models import Ingredients
 from foodhack.helpers import RequestDict
 
 
+@csrf_protect
 def get_random_item(requests):
     """
     Получить рандомные ингридиенты
